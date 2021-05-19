1f958df703ced85cf8e4e7181121b13e89f0ade1
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-05-19 12:05:16 -0400

    #10 Create a Category

--- TabloidMVC/Controllers/CategoryController.cs
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -39,15 +39,17 @@ namespace TabloidMVC.Controllers
         // POST: CategoryController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Category category)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                _categoryRepository.Add(category);
+
+                return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return View(category);
             }
         }
 
